5098852d1b2573473b89f30fc888e81d8842546f
Jasmin <116222536+JsySr@users.noreply.github.com>
2024-04-18 10:22:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
-    // Función para obtener y mostrar la última orden de MockAPI
-    function obtenerUltimaOrden() {
+    // Función para obtener y mostrar los últimos 3 registros de MockAPI
+    function obtenerUltimosRegistros() {
         $.getJSON('https://66176aa2ed6b8fa43482988c.mockapi.io/Voz', function(data){
-            // Obtener la última orden
-            var lastOrder = data[data.length - 1];
             // Limpiar el contenido anterior de la lista desordenada
             $('#ultimaOrdenLista').empty();
-            // Agregar el último dato como un elemento de la lista
-            $('#ultimaOrdenLista').append('<li>' + lastOrder.text + '</li>');
+            // Obtener los últimos 3 registros y agregarlos al HTML
+            for (var i = data.length - 3; i < data.length; i++) {
+                var order = data[i];
+                $('#ultimaOrdenLista').append('<li>' + order.accion + ' - ' + order.fecha + '</li>');
+            }
         });
     }
 
-    // Llamar a la función para obtener la última orden cada 2 segundos
-    setInterval(obtenerUltimaOrden, 2000);
+    // Llamar a la función para obtener los últimos 3 registros cada 2 segundos
+    setInterval(obtenerUltimosRegistros, 2000);
 });
